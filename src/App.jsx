2752cccdbebf24dbcd63b11774f606c95130d6af
Chaosgale <yantus@gmail.com>
2024-06-08 22:57:00 -0500

    installed Mantine library

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,25 @@ import Footer from "./components/Footer";
 import Page from "./components/Page";
 import { useLocation } from "react-router-dom";
 
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+
 function App() {
   const currentPage = useLocation().pathname;
 
   return (
-    <div>
-      <Header>
-        <Nav currentPage={currentPage} />
-      </Header>
-      <main>
-        <Page currentPage={currentPage} />
-      </main>
-      <Footer />
-    </div>
+    <MantineProvider>
+      <div>
+        <Header>
+          <Nav currentPage={currentPage} />
+        </Header>
+        <main>
+          <Page currentPage={currentPage} />
+        </main>
+        <Footer />
+      </div>
+    </MantineProvider>
   );
 }
 
