9a6391ac629a2c49ae1980dd38ab4b3be673a21c
yan.ashe <145398026+Chaosgale@users.noreply.github.com>
2024-06-17 18:31:01 -0500

    Update index.jsx

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -6,7 +6,7 @@ function Profile() {
             <div className={classes.submain}>
                 <h6 className={classes.sublogo}>ASPIRING SOFTWARE DEVELOPER</h6>
                 <h1 className={classes.logo}>Yan Ashe</h1>
-                <img src="https://drive.google.com/file/d/1YvA8MF2_VoE1tWJPNfgQVb4m2_VSeAe3/view?usp=sharing" alt="Guy in the jacket" width="400" height="600" />
+                <img src="https://raw.githubusercontent.com/Chaosgale/ReactPortfolio/main/src/assets/pictures/portrpic.jpeg" alt="Guy in the jacket" width="400" height="600" />
 
             </div>
         </div>
